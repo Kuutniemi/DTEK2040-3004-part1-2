3517e04568d3fd5b72ecfb989a8c6af935703527
Jkuut <jekuut@utu.fi>
2022-02-02 18:05:32 +0200

    Teht 1.5  Valmis

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -22,7 +22,7 @@ const Contents =(props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>{props.exc[0].exercises + props.exc[1].exercises + props.exc[0].exercises} </p>
+      <p>{props.exc[0].exercises + props.exc[1].exercises + props.exc[2].exercises} </p>
     </div>
   )
 }
@@ -36,27 +36,29 @@ const Part =(props) => {
 }
 
 const App = () => {
-  const course = 'Superadvanced web and mobile programming'
-  const parts = [
-    {
-      name: 'Basics of React',
-      exercises: 8
-    },
-    {
-      name: 'Using props',
-      exercises: 10
-    },
-    {
-      name: 'Component states', 
-      exercises: 12
-    }
-  ]
+  const course = {
+    name: 'Superadvanced web and mobile programming',
+    parts: [
+      {
+        name: 'Basics of React',
+        exercises: 8
+      },
+      {
+        name: 'Using props',
+        exercises: 10
+      },
+      {
+        name: 'Component states',
+        exercises: 12
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header kurssi={course}/>
-      <Contents osa={parts} teht={parts}/>
-      <Total exc={parts} />
+      <Header kurssi={course.name}/>
+      <Contents osa={course.parts} teht={course.parts}/>
+      <Total exc={course.parts} />
     </div>
   )
 }
