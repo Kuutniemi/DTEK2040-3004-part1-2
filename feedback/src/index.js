b4ac2b0964fe8c0b81066a141b94f3400545cd49
Kuutniemi <jkuutniemi@outlook.com>
2022-03-24 20:00:46 +0200

    Vitusti keske

--- feedback/src/index.js
+++ feedback/src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
+
+const Buttons = (props) => {
+  return (
+    <div>
+      <button type="button" onClick={()=> props.arvo[0].summa.setState({summa: props.arvo[0].summa +1})}>{props.nappula[0].name}</button>
+      <button type="button">{props.nappula[1].name}</button>
+      <button type="button">{props.nappula[2].name}</button>
+    </div>
+  )
+}
+
+const Statistiikka = (props) => {
+  return (
+    <h1>Statistiikka...</h1>
+    
+  )
+}
+
+const App = () => {
+  const data = {
+    name: 'Anna palautetta',
+    parts: [
+      {
+        name: 'hyvä',
+        value: 1,
+        summa: 0,
+      },
+      {
+        name: 'neutraali',
+        value: 0,
+        summa: 0,
+      },
+      {
+        name: 'huono',
+        value: -1,
+        summa: 0,
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header title={data.name}/>
+      <Buttons nappula={data.parts}/>
+      <Statistiikka arvo={data.parts}/>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
