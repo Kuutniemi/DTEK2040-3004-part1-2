216f438a0ca23c701c0c9d92f939788c4c265a21
Jouni Kuutniemi <jkuutniemi@outlook.com>
2022-03-28 01:42:29 +0300

    1.6 ei toimi

--- feedback/src/index.js
+++ feedback/src/index.js
@@ -19,18 +19,17 @@ const Buttons = (props) => {
     <div>
       <h1>Anna palautetta</h1>
       <div>
-     {
-       props.data.map(function (parts, index){
-         return (
-           <div key={index}>
-             <button type='button' onClick={() => props.aseta(5)}>{parts.name}</button>
-           </div>
-         )
-       })
-     }
-   </div>
+        {props.data.map(function (parts, index) {
+          return (
+            <div key={index}>
+              <button type="button" onClick={() => props.lisaa(index)}>
+                {parts.name}
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
-    
   );
 };
 
@@ -75,25 +74,75 @@ class Apps extends React.Component {
         {
           name: "huono",
           value: -1,
-          summa: 0,
+          summa: 4,
         },
       ],
     };
+    const joku = (props) => {
+      const initialState = {
+        name: String,
+        value: Number,
+        summa: Number,
+      };
+
+      function reducer(state = initialState, action) {
+        switch (action.type) {
+          case "VAIHDA":
+            return {
+              ...state,
+              summa: action.summa,
+            };
+          default:
+            return state;
+        }
+      }
+    };
   }
+
+  lisaa1 = (arvo) => {
+    console.log("lisaa1", arvo);
+    console.log(this.state);
+    //console.log(this.state.parts);
+    return this.setState(
+      ...this.state,
+      {
+        parts: [...this.state, { summa: this.state.parts[arvo].summa + 1 }],
+      },
+      console.log(this.state),
+      console.log(this.state.summa)
+    );
+  };
+
+  nimea = (arvo) => {
+    console.log(this.state.name);
+    console.log(this.state);
+    return this.setState({
+      name: (this.state.name = arvo),
+    });
+  };
+
   asetaArvoon = (arvo) => {
-    console.log(this.state.parts)
-    return () => {
-      this.setState({parts: {summa: this.state.data.summa + arvo}})
-      console.log(this.state.parts)
-    }
-  }
+    console.log(this.state.parts[arvo].name, this.state.parts[arvo].summa);
+    console.log(this.state);
+    return this.setState([
+      {
+        parts: [{ summa: this.state.parts[arvo].summa + 1 }],
+      },
+    ]);
+  };
   render() {
-    return(
+    return (
       <div>
-        <Buttons data={this.state.parts} aseta={this.asetaArvoon}/>
-        <Statistiikka data={this.state.parts}/>
+        <Buttons
+          data={this.state.parts}
+          aseta={this.asetaArvoon}
+          lisaa={this.lisaa1}
+          nimea={this.nimea}
+          reducer={""}
+        />
+        <Statistiikka data={this.state.parts} />
       </div>
-    )
+    );
   }
 }
 
